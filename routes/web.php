88dc82aa3abd65b5ced2e761a810cf5685331ba1
Humayun Ahmed <humayun11998@gmail.com>
2026-04-11 16:38:18 +0500

    feat: dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\AuthMiddleware;
+use App\Http\Middleware\CheckUserMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
 Route::controller(AuthController::class)->group(function () {
-    Route::get('/', 'loginPage')->name('login.page');
-    Route::get('/dashboard', 'Dashboard')->name('dashboard');
-
+    Route::get('/', 'loginPage')->name('login.page')->middleware(CheckUserMiddleware::class);
     Route::post('user/login', 'userLogin')->name('user.login');
     Route::post('user/logout', 'userLogout')->name('user.logout');
+
+    Route::middleware(AuthMiddleware::class)->group(function () {
+        Route::get('/dashboard', 'Dashboard')->name('dashboard');
+    });
 });
